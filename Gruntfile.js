b49d2a4b79a4f408a21b81b918c9083a6bfd828e
Saskia Falkenhagen <saskia.falkenhagen@gmail.com>
2024-10-13 17:57:48 +0200

    feat: refactor Gruntfile to load task configurations dynamically

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,32 @@
 module.exports = function(grunt) {
+  function loadConfig(path) {
+    const glob = require('glob');
+    const object = {};
+    let key;
 
-  // Project configuration.
-  grunt.initConfig({
+    glob.sync('*', {cwd: path}).forEach(function(option) {
+      key = option.replace(/\.js$/, '');
+      object[key] = require(path + option);
+    });
+
+    return object;
+  }
+
+  const config = {
     pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
-      }
-    }
-  });
+    env: process.env
+  };
 
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.util._.extend(config, loadConfig('./tasks/options/'));
+  setTimeout(() => {
+    grunt.initConfig(config);
+  }
+  , 0);
+
+  require('load-grunt-tasks')(grunt);
+
+  grunt.registerTask('default', ['build']);
+  grunt.registerTask('build', ['clean', 'copy', 'sass', 'uglify']);
+  grunt.registerTask('serve', ['build', 'connect', 'watch']);
+  grunt.registerTask('deploy', ['build', 'gh-pages']);
 };
